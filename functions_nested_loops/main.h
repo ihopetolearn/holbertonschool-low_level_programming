aa832ccbf0d7d2a821e31d9047ad682a6764f6e8
ihopetplearn <arifpaiman9@gmail.com>
2023-05-30 14:49:04 -0700

    main.h

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+int _putchar(char c);
+void print_alphabet(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauser(void);
+void time_table(void);
+int add(int ,int);
+void print_to_98(int n);
+void print_times_table(int n);
