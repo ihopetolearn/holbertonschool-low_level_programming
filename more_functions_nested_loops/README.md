dbebab166b7fb0d15448396b230d8488072c24b6
ihopetplearn <arifpaiman9@gmail.com>
2023-05-31 20:49:16 -0700

    sfsaf

---
+++ more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+#this is all about the function and tell  us that hiw we can solveit 
---
+++ more_functions_nested_loops/_putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"                                                                                                                                                           
+#include <unistd.h>                                                                                                                                                         
+ /**                                                                                                                                                                        
+  * the putchar print the character c                                                                                                                                       
+  * sdfjskjf                                                                                                                                                                
+ */                                                                                                                                                                                                                                                                                                                                                     
+int _putchar(char c)                                                                                                                                                        
+{                                                                                                                                                                           
+	        return (write(1, &c, 1));                                                                                                                                           
+}                                  
