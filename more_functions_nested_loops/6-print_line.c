d59615610a55c9e6000c444f1d721944705b68cc
ihopetplearn <arifpaiman9@gmail.com>
2023-06-01 13:37:54 -0700

    asfa

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,19 @@
-
 #include "main.h"
 
- /**
-  * file: print_line
-  * auth: arifpaiman
-  * print_line - Draws a straight line using the character _.
-  * @n: The number of _ characters to be printed.
-  */
+/**
+ * print_line - Draws a straight line according to parameter
+ * @n: The number of lines to draw
+ * Return: empty
+ */
 void print_line(int n)
 {
-	int len;
+	int x;
 
-	if (n > 0)
-	{
-	for (len = 0; len < n; len++)
-	_putchar('_');
-	}
+	if (n <= 0)
+	_putchar('\n');
+	else
+	for (x = 0; x < n; x++)
+	_putchar(95);
 	_putchar('\n');
 }
+
